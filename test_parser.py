c59cf4ef9372bc144e12821139cd1f166a2f4388
Gubenko_Daniil <gubenko@ia-group.ru>
2024-06-28 13:50:53 +0300

    Рефакторинг тестов

--- test_parser.py
+++ test_parser.py
@@ -1,152 +1,41 @@
 import time
+from typing import Union, Literal, Callable, Optional
 
 from general import NavOption, NavItem
 from parser import DaisyParser
 
 
+def _test_nav_option(parser: DaisyParser, audio_path: str, current_time: float, nav_option: NavOption, direction: Union[Literal[1], Literal[-1]] = 1):
+    func: Callable[[str, float], Optional[NavItem]] = parser.get_next if direction == 1 else parser.get_prev
+    parser.set_nav_option(nav_option)
+
+    start_time = time.time()
+    nav_item_1: NavItem = func(audio_path, current_time)
+    if nav_item_1:
+        print(nav_item_1.__dict__)
+        nav_item_2: NavItem = func(nav_item_1.audio_path, nav_item_1.start_time)
+        if nav_item_2:
+            print(nav_item_2.__dict__)
+            nav_item_3: NavItem = func(nav_item_2.audio_path, nav_item_2.start_time)
+            if nav_item_3:
+                print(nav_item_3.__dict__)
+                end_time = time.time()
+                print(f"Время между 3 запросами: {end_time - start_time}\n")
+
+
 def test_book(folder_path: str, audio_path: str, current_time: float):
     parser = DaisyParser(folder_path)
 
-    parser.set_nav_option(NavOption.HEADING)
-
-    start_time = time.time()
-    nav_heading_1: NavItem = parser.get_next(audio_path, current_time)
-    print(nav_heading_1.__dict__)
-    nav_heading_2: NavItem = parser.get_next(nav_heading_1.audio_path, nav_heading_1.start_time)
-    print(nav_heading_2.__dict__)
-    nav_heading_3: NavItem = parser.get_next(nav_heading_2.audio_path, nav_heading_2.start_time)
-    print(nav_heading_3.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    start_time = time.time()
-    nav_heading_3: NavItem = parser.get_prev(audio_path, current_time)
-    print(nav_heading_3.__dict__)
-    nav_heading_2: NavItem = parser.get_prev(nav_heading_3.audio_path, nav_heading_3.start_time)
-    print(nav_heading_2.__dict__)
-    nav_heading_1: NavItem = parser.get_prev(nav_heading_2.audio_path, nav_heading_2.start_time)
-    print(nav_heading_1.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    parser.set_nav_option(NavOption.PHRASE)
-
-    start_time = time.time()
-    nav_phrase_1: NavItem = parser.get_next(audio_path, current_time)
-    print(nav_phrase_1.__dict__)
-    nav_phrase_2: NavItem = parser.get_next(nav_phrase_1.audio_path, nav_phrase_1.start_time)
-    print(nav_phrase_2.__dict__)
-    nav_phrase_3: NavItem = parser.get_next(nav_phrase_2.audio_path, nav_phrase_2.start_time)
-    print(nav_phrase_3.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    start_time = time.time()
-    nav_phrase_2: NavItem = parser.get_prev(audio_path, current_time)
-    print(nav_phrase_2.__dict__)
-    nav_phrase_1: NavItem = parser.get_prev(nav_phrase_2.audio_path, nav_phrase_2.start_time)
-    print(nav_phrase_1.__dict__)
-    nav_phrase_0: NavItem = parser.get_prev(nav_phrase_1.audio_path, nav_phrase_1.start_time)
-    print(nav_phrase_0.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    parser.set_nav_option(NavOption.PAGE)
-
-    start_time = time.time()
-    nav_page_1: NavItem = parser.get_next(audio_path, current_time)
-    print(nav_page_1.__dict__)
-    nav_page_2: NavItem = parser.get_next(nav_page_1.audio_path, nav_page_1.start_time)
-    print(nav_page_2.__dict__)
-    nav_page_3: NavItem = parser.get_next(nav_page_2.audio_path, nav_page_2.start_time)
-    print(nav_page_3.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    # start_time = time.time()
-    # nav_page_3: NavText = parser.get_prev(audio_path, current_time)
-    # print(nav_page_3.__dict__)
-    # nav_page_2: NavText = parser.get_prev(nav_page_3.audio_path, nav_page_3.start_time)
-    # print(nav_page_2.__dict__)
-    # nav_page_1: NavText = parser.get_prev(nav_page_2.audio_path, nav_page_2.start_time)
-    # print(nav_page_1.__dict__)
-    # end_time = time.time()
-    # print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-
-def test_book_v3(folder_path: str, audio_path: str, current_time: float):
-    parser = DaisyParser(folder_path)
-
-    parser.set_nav_option(NavOption.PAGE)
-
-    start_time = time.time()
-    nav_page_1: NavItem = parser.get_next(audio_path, current_time)
-    print(nav_page_1.__dict__)
-    nav_page_2: NavItem = parser.get_next(nav_page_1.audio_path, nav_page_1.start_time)
-    print(nav_page_2.__dict__)
-    nav_page_3: NavItem = parser.get_next(nav_page_2.audio_path, nav_page_2.start_time)
-    print(nav_page_3.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    start_time = time.time()
-    nav_page_3: NavItem = parser.get_prev(audio_path, current_time)
-    print(nav_page_3.__dict__)
-    nav_page_2: NavItem = parser.get_prev(nav_page_3.audio_path, nav_page_3.start_time)
-    print(nav_page_2.__dict__)
-    nav_page_1: NavItem = parser.get_prev(nav_page_2.audio_path, nav_page_2.start_time)
-    print(nav_page_1.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    parser.set_nav_option(NavOption.HEADING)
-
-    start_time = time.time()
-    nav_heading_1: NavItem = parser.get_next(audio_path, current_time)
-    print(nav_heading_1.__dict__)
-    nav_heading_2: NavItem = parser.get_next(nav_heading_1.audio_path, nav_heading_1.start_time)
-    print(nav_heading_2.__dict__)
-    nav_heading_3: NavItem = parser.get_next(nav_heading_2.audio_path, nav_heading_2.start_time)
-    print(nav_heading_3.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    start_time = time.time()
-    nav_page_3: NavItem = parser.get_prev(audio_path, current_time)
-    print(nav_page_3.__dict__)
-    nav_page_2: NavItem = parser.get_prev(nav_page_3.audio_path, nav_page_3.start_time)
-    print(nav_page_2.__dict__)
-    nav_page_1: NavItem = parser.get_prev(nav_page_2.audio_path, nav_page_2.start_time)
-    print(nav_page_1.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    parser.set_nav_option(NavOption.PHRASE)
-
-    start_time = time.time()
-    nav_phrase_1: NavItem = parser.get_next(audio_path, current_time)
-    print(nav_phrase_1.__dict__)
-    nav_phrase_2: NavItem = parser.get_next(nav_phrase_1.audio_path, nav_phrase_1.start_time)
-    print(nav_phrase_2.__dict__)
-    nav_phrase_3: NavItem = parser.get_next(nav_phrase_2.audio_path, nav_phrase_2.start_time)
-    print(nav_phrase_3.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
-
-    start_time = time.time()
-    nav_phrase_2: NavItem = parser.get_prev(audio_path, current_time)
-    print(nav_phrase_2.__dict__)
-    nav_phrase_1: NavItem = parser.get_prev(nav_phrase_2.audio_path, nav_phrase_2.start_time)
-    print(nav_phrase_1.__dict__)
-    nav_phrase_0: NavItem = parser.get_prev(nav_phrase_1.audio_path, nav_phrase_1.start_time)
-    print(nav_phrase_0.__dict__)
-    end_time = time.time()
-    print(f"Время между 3 запросами: {end_time - start_time}\n")
+    _test_nav_option(parser, audio_path, current_time, NavOption.HEADING, 1)
+    _test_nav_option(parser, audio_path, current_time, NavOption.HEADING, -1)
+    _test_nav_option(parser, audio_path, current_time, NavOption.PHRASE, 1)
+    _test_nav_option(parser, audio_path, current_time, NavOption.PHRASE, -1)
+    _test_nav_option(parser, audio_path, current_time, NavOption.PAGE, 1)
+    _test_nav_option(parser, audio_path, current_time, NavOption.PAGE, -1)
 
 
 FRONTPAGE = ['frontpage', '823_r.mp3', 456.5]
 TEST_BOOK = ['test_book', '08_26th_.mp3', 263]
 DAISY_3 = ['daisy_3', 'speechgen0007.mp3', 13]
 
-# test_book(*FRONTPAGE)
-test_book_v3(*DAISY_3)
+test_book(*TEST_BOOK)
